d7fc806d6ed0bc32f43ef643fe0de28170daa11c
mrSakur <mrsakur@mail.ru>
2022-06-11 19:09:40 +0300

    Fix

--- apps/search/static/search/js/search.js
+++ apps/search/static/search/js/search.js
@@ -3,7 +3,12 @@ window.addEventListener('DOMContentLoaded', event=>{
 
 const sendBtn = document.querySelector('#button-search'),
     searchInput = document.querySelector('#search-input')
+     datatablesSimple = document.getElementById('datatablesSimple');
+     errorModal = document.getElementById('error-modal');
 
+
+
+let dataTable = new simpleDatatables.DataTable(datatablesSimple)
 
   function renderYesNo(data, cell, row) {
         if (data == 'true') {
@@ -17,7 +22,9 @@ const sendBtn = document.querySelector('#button-search'),
 
 function sendRequests() {
 
-    if (searchInput.value.trim() == '') return;
+    if (searchInput.value.trim() == '') {
+    showErrorModal('Пустая строка запроса')
+    return;}
 
     let data = {search_term:searchInput.value}
 
@@ -32,26 +39,36 @@ function sendRequests() {
             return
         }
 
-        const datatablesSimple = document.getElementById('datatablesSimple');
-
         if (datatablesSimple) {
 
             let headings = Object.keys(data.results[0])
-            headings.unshift('select')
+            headings.unshift('<input type="checkbox">')
             data = data.results.map(item=>Object.values(item))
 
             data.forEach(item => {item.unshift('false')})
 
-            let dataTable = new simpleDatatables.DataTable(datatablesSimple,{
+            dataTable.destroy()
+
+            dataTable = new simpleDatatables.DataTable(datatablesSimple,{
                 data: {
                     headings: headings,
                     data: data
                 },
-                paging: false,
+                fixedHeight: true,
                 columns: [{
                     select: 0,
-                    render: renderYesNo
+                    render: renderYesNo,
+                    sortable: false
                 }]
+            });
+
+            dataTable.headings[0].addEventListener('click', () => {
+
+                Array.from(dataTable.data).forEach(item =>
+                {
+                    item.cells[0].childNodes[0].checked = true
+                });
+
             });
 
 
@@ -63,6 +80,32 @@ function sendRequests() {
 
 sendBtn.addEventListener('click', () => sendRequests());
 
+function showErrorModal(error) {
 
+    const modal = document.createElement('div');
+    modal.classList.add('hide');
+    modal.innerHTML = `
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">\
+<div class="modal-dialog" role="document"> \
+        <div class="modal-content"> \
+            <div class="modal-header"> \
+                <h5 class="modal-title" id="exampleModalLabel">Внимание</h5> \
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button> \
+            </div> \
+            <div class="modal-body"> \
+                Подтверждаете обработку данных ? \
+            </div> \
+            <div class="modal-footer"> \
+                <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Отменить</button> \
+                <button class="btn btn-primary" type="button">Подтвердить</button> \
+            </div> \
+        </div> \
+</div>
+</div>
+`
+
+$('#error-modal').modal()
+
+}
 }
 );
